869db6fdcff65ddd0f73ccf814e310028134f0a5
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-07-04 12:42:21 +0530

    added cutom scroll bar

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,11 +2,10 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router";
 
-
 const Detail = () => {
   const { id } = useParams();
 
-  const {posts} = useSelector((state)=> state.posts)
+  const { posts } = useSelector((state) => state.posts);
   const item = posts.find((item) => item.id === Number(id));
 
   if (!item) {
@@ -15,12 +14,21 @@ const Detail = () => {
   return (
     <div className="detailDiv">
       <h1>Detail Page</h1>
-      <img src={`https://picsum.photos/200?random=${posts.id}`} alt='cardImg' />
-      <h2><b>Title:</b> {item.title}</h2>
-      <p><b>Body:</b> {item.body}</p>
-      <p><b>User ID:</b> {item.id}</p>
+      <img src={`https://picsum.photos/200?random=${posts.id}`} alt="cardImg" />
+      <h2>
+        <b>Title:</b> {item.title}
+      </h2>
+      <p>
+        <b>Body:</b> {item.body}
+      </p>
+      <p>
+        <b>User ID:</b> {item.id}
+      </p>
 
-      <p style={{color:'red'}}>* On clicking the readmore the image could be different but content is same.</p>
+      <p style={{ color: "red" }}>
+        * On clicking the readmore the image could be different but content is
+        same.
+      </p>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,43 +1,42 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {getPosts} from '../features/getDataSlice';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPosts } from "../features/getDataSlice";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   // const [readMore,setReadMore] = useState(false);
 
   const dispatch = useDispatch();
-  const {posts,loading} = useSelector((state)=> state.posts)
+  const { posts, loading } = useSelector((state) => state.posts);
 
   useEffect(() => {
-    dispatch(getPosts())
-  },[dispatch]);
+    dispatch(getPosts());
+  }, [dispatch]);
 
-  if(loading) return <div class="spinner"></div>
+  if (loading) return <div class="spinner"></div>;
   return (
     <div>
-        <h2>Social Media App</h2>
-        <div className='cardContainer'>
-        {
-          posts.map((post) => (
-            <div className='card' key={post.id}>
-           <img src={`https://picsum.photos/200?${post.id}`} alt='cardImg' />
-              <p>User ID: {post.id}</p>
-              <p>Title: {post.title.slice(0,15)}...</p>
-              <p>Body: {post.body.slice(0,30)}
-              
-              <Link to={`/detail/${post.id}`}> &nbsp;
-                 <span style={{color:'white',fontWeight:'800'}}>Read More...</span>
+      <p style={{fontSize:'30px',paddingBottom:'1rem',borderBottom:'1px solid white'}}>Social Media App</p>
+      <div className="cardContainer">
+        {posts.map((post) => (
+          <div className="card" key={post.id}>
+            <img src={`https://picsum.photos/200?${post.id}`} alt="cardImg" />
+            <p>User ID: {post.id}</p>
+            <p>Title: {post.title.slice(0, 15)}...</p>
+            <p>
+              Body: {post.body.slice(0, 30)}
+              <Link to={`/detail/${post.id}`}>
+                {" "}
+                <span style={{ color: "white"}}>
+                  Read More...
+                </span>
               </Link>
-
-              </p>
-              
-            </div>
-          ))
-        }
-        </div>
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
